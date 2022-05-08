aecabb84e9d7f1b1d22066515b08adc1cca44177
Josef <josef.prinz@inpro.de>
2022-05-08 16:56:08 +0200

    new example added

---
+++ PlugInDevelopment_V6/Aml.Editor.Plugin.Collada/Plugin.cs
@@ -0,0 +1,130 @@
+﻿using Aml.Editor.Plugin.Base;
+using Aml.Editor.Plugin.Contract.Theming;
+using Aml.Editor.Plugin.Contracts;
+using Aml.Editor.Plugin.Contracts.Commanding;
+using Aml.Engine.AmlObjects;
+using Aml.Engine.CAEX;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Aml.Editor.Plugin.Window
+{
+    /// <summary>
+    /// This plugin defines its own mainw window. It supports a zooming UI.
+    /// </summary>
+    [ExportMetadata("Author", "Josef Prinz")]
+    [ExportMetadata("Owner", "AutomationML")]
+    [ExportMetadata("DisplayName", "COLLADA Viewer")]
+    [ExportMetadata("Description",
+        "This plugin does not provide a dockable view but implements its own main window to visualize collada models .")]
+    [Export(typeof(IAMLEditorPlugin))]
+    public class Plugin : PluginBase, ISupportsUIZoom, ISupportsThemes, IEditorCommanding, IAMLEditorExternalsPlugin
+    {
+        private PluginView _mainWindow;
+        private ApplicationTheme _currentThem;
+        private double _zoom = 1.0;
+
+        public Plugin()
+        {
+            DisplayName = "COLLADA Viewer";
+        }
+
+        protected override void ActivateCommandExecute(object parameter)
+        {
+            base.ActivateCommandExecute(parameter);
+            _mainWindow = new PluginView();
+            _mainWindow.ZoomFactor = _zoom;
+            _mainWindow.Closed += OnMainWindowClosed;
+            _mainWindow.Show();
+            OnThemeChanged (_currentThem);  
+        }
+
+        protected override void TerminateCommandExecute(object parameter)
+        {
+            base.TerminateCommandExecute(parameter);
+            if (parameter is bool isClosing && isClosing)
+            {
+                return;
+            }
+            _mainWindow?.Close();
+        }
+
+        private void OnMainWindowClosed(object sender, EventArgs e)
+        {
+            if (IsActive)
+            {
+                TerminateCommandExecute (true);
+            }
+        }
+
+        public override bool IsReactive => true;
+
+        public override bool IsReadonly => false;
+
+        public override string PackageName => "Aml.Editor.Plugin.Window";
+
+        public CommandExecution EditorCommand { get; set; }
+
+        public bool IsExternalsViewer => throw new NotImplementedException();
+
+        public string MIMEType => throw new NotImplementedException();
+
+        public override void ChangeAMLFilePath(string amlFilePath)
+        {
+        }
+
+        public override void ChangeSelectedObject(CAEXBasicObject selectedObject)
+        {
+        }
+
+        public override void PublishAutomationMLFileAndObject(string amlFilePath, CAEXBasicObject selectedObject)
+        {
+        }
+
+        public void OnUIZoomChanged(double zoomFactor)
+        {
+            if (_mainWindow != null)
+            {
+                _mainWindow.ZoomFactor = zoomFactor;
+            }
+            _zoom = zoomFactor;
+        }
+
+        public void OnThemeChanged(ApplicationTheme theme)
+        {
+            if (_mainWindow == null)
+            {
+                return;
+            }
+            _currentThem = theme;
+            // detect, which theme is currently used by mahapps.metro
+            var applicationTheme = ControlzEx.Theming.ThemeManager.Current.DetectTheme(Application.Current);
+            if (applicationTheme != null)
+            {
+                // change the theme for used toolkit elements. This method must be called in all xaml views
+                // where aml.toolkit or aml.skins resources are used.
+                ThemeManager.ChangeTheme(_mainWindow.Resources,
+                    applicationTheme.BaseColorScheme);
+
+                // change the theme for the used mahapps.metro ui elements
+                ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this, _mainWindow.Resources, applicationTheme);
+            }
+        }
+
+        public void ViewExternal(RefURIAttributeType attribute, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ViewExternal(RefURIAttributeType attribute, Stream memoryStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlugInDevelopment_V6/README.md
@@ -0,0 +1,94 @@
+## Development and deployment of plug-ins for the AutomationML Editor as of version 6.0
+
+------
+
+### Prerequisites
+
+- You need AutomationML Editor version 6.0 or higher to use the plug-ins with the editor. For development and testing the sandbox project is sufficient.
+- You need at least Visual Studio 2019 as your development platform. The projects in this repository were created using Visual Studio 2022. 
+- Install the .NET 5 SDK
+
+### What's new for Version 6.0
+
+- The example projects are switched to the SDK based **.csproj* format for Visual Studio projects.  
+- The Target Framework has changed to .NET 5. 
+- New Resources, provided for AutomationML Editor Plug-In Development are used. This includes, for example, the possibility of selecting the light or dark display mode.
+
+### Examples
+
+The examples show different aspects of plug-in development. The plug-in contract provides specific interface classes for individual aspects. These can be implemented in a plug-in individually or in combination. The examples use different docking modes for the embedding of the views in the editor's UI layout.
+
+- `Aml.Editor.Plugin.Theming` 
+  This example shows how a plugin can use the light or dark display mode currently selected in the editor. The Theme selection aspect is included in all other plugin examples, too.  The implemented contract is:
+
+  - `ISupportsThemes`
+
+    
+
+- `Aml.Editor.Plugin.WithToolbar`
+  This example shows how a plugin can add a toolbar defining Plugin commands to the Editor's main toolbar. The implemented contracts are:
+
+  - `ISupportsThemes`
+
+  - `IToolBarIntegration`
+
+    
+
+- `Aml.Editor.Plugin.MultiView`
+  In this example, a plugin is developed that defines multiple views that are all embedded in the current layout by the editor's docking system. The implemented contracts are:
+
+  - `ISupportsThemes`
+
+  - `IAMLEditorViewCollection`,
+
+  - `INotifyViewActivation`
+
+    
+
+- `Aml.Editor.Plugin.CallCommand`
+  This example shows how a plugin can activate user methods of the editor like opening an AutomationML file. The implemented contracts are:
+
+  - `ISupportsThemes`
+
+  - `IEditorCommanding`
+
+    
+
+- `Aml.Editor.Plugin.Window`
+  This plugin creates a separate main window that is not integrated into the UI layout of the editor. this window reacts to the UI scaling of the editor. The implemented contracts are:
+
+  - `ISupportsThemes`
+
+  - `ISupportsUIZoom`
+
+    
+
+- `Aml.Editor.Plugin.Collada`
+  This example shows how a plugin processes the selection of an object that references an external COLLADA file. This plugin das dependencies to other packages which are effected by the <EnableDynamicLoading>true</EnableDynamicLoading> element. The implemented contracts are:
+
+  - `ISupportsThemes`
+
+  - `ISupportsUIZoom`
+
+  - `IAMLEditorExternalsPlugin`
+
+  - `IEditorCommanding`
+
+    
+
+### Publishing
+
+If you want to share the created plug-in for third party use you need to create a package. In between the `<PropertyGroup>` tags of the project file, the <EnableDynamicLoading>true</EnableDynamicLoading> element has to be added. It prepares the project so that it can be used as a plugin. Among other  things, this will copy all of its dependencies to the output of the  project.
+
+Packages referenced by the AutomationML Editor should have the  <ExcludeAssets>runtime</ExcludeAssets> element added to the Package reference tag. This will not include the shared packages into the package.
+
+The Tag *AMLEditorPlugin* has to be added to the package tag names. This ensures, that the Package Manager can find the plugin in a provided package repository and can offer it to other users.
+
+The *PackageName* of the Plugin shall be unique.
+
+
+
+
+
+
+
